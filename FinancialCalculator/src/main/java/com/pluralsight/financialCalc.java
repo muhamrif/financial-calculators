19d5075afe179470c80033199a533ddd22d34a7a
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-10-11 22:16:06 -0400

    new methods created

---
+++ FinancialCalculator/src/main/java/com/pluralsight/financialCalc.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class financialCalc {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+    }
+
+
+    public static int mortgageCalc (){
+       return 1;
+    }
+
+    public static int futureVal(){
+      return 1;
+    }
+
+    public static int presentVal(){
+        return 1;
+    }
+
+}
